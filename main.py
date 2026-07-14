353f71fae8220dc1b7da2da570e40d463a9757eb
neilexe <neilbanerjee@Neils-MacBook-Pro.local>
2026-07-14 10:40:26 +0530

    feat: add pipeline error handling

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ from src.logger import logger
 
 logger.info("Starting API Data Pipeline")
 
-users = fetch_users()
+try:
+    users = fetch_users()
 
-users_df = transform_users(users)
+    users_df = transform_users(users)
 
-save_raw_users(users)
-save_processed_users(users_df)
+    save_raw_users(users)
+    save_processed_users(users_df)
 
-logger.info("Pipeline completed successfully")
+    logger.info("Pipeline completed successfully")
 
+except Exception as e:
+    logger.error(f"Pipeline failed: {e}")
